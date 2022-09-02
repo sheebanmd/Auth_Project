69880080b73e26ecc016877ff0dc3dac986c63c5
sheebanmd <91521103+sheebanmd@users.noreply.github.com>
2022-09-03 00:56:47 +0530

    Auth_Project_Done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import AuthContext from './store/auth-context';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
+
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
-        </Route>
-        <Route path='/auth'>
+       </Route>
+        {!isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+        {isLoggedIn && <Route path='/profile'>
           <UserProfile />
+        </Route>}
+        <Route to="*">
+          <Redirect to="/"></Redirect>
         </Route>
       </Switch>
     </Layout>
